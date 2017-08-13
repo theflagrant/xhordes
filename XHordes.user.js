c719f2e4c46680eb52f851f9681be26bff24d3fd
LegusX <loghenrie@gmail.com>
2017-08-12 19:30:50 -0500

    fixed

--- XHordes.user.js
+++ XHordes.user.js
@@ -4,17 +4,11 @@
 // @version      0.0.1
 // @description  A modded version of the Hordes.io client
 // @author       LegusX
-// @match        http://hordes.io
-// @grant        none
+// @match        http://hordes.io/*
+// @grant        GM_getValue
+// @grant        GM_setValue
 // ==/UserScript==
-
-(function() {
-    'use strict';
-    var allScripts = document.getElementsByTagName("script");
-    for (var i = allScripts.length; i--; ) {
-        allScripts[i].remove();
-    }
     var script = document.createElement("script");
-    script.src = "https://raw.githubusercontent.com/LegusX/xhordes/master/0-0-1.js";
+    script.src = "https://drive.google.com/uc?export=download&id=0By1TAGAKPvu4Z01ZSEhfcHFUODQ";
+    script.dataType = "text/javascript";
     document.body.appendChild(script);
-})();
